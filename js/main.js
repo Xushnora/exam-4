ce327aff942ee094a00771cceaf8146c63b44e08
Xushnora <xushnoranishonova@gmail.com>
2022-04-04 23:20:49 -0700

    changed

--- js/main.js
+++ js/main.js
@@ -1,44 +1,51 @@
+
 const stories = [
     {
         id: 1,
         name: "master-class",
-        media:  ["imgs/tipografika.3.jpg", "imgs/tipografika2.jpg"],
+        media:  ["imgs/active.img1.jpg", "imgs/tipografika2.jpg"],
         avatar: "imgs/img1.jpg",
+        type: 'imgs',
         active: false
     },
     {
         id: 2,
         name: "Imtihon",
-        media: ["imgs/tipografika2.jpg", "imgs/tipografika2.jpg"],
+        media: ["media/MUSOBAQA tgg(Android).mp4", "media/MUSOBAQA tgg(Android).mp4"],
         avatar: "imgs/img2.jpg",
+        type: 'vedio',
         active: false
     },
     {
         id: 3,
         name: "ramazon",
-        media: ["imgs/tipografika1.jpg", "imgs/tipografika1.jpg"],
+        media: ["imgs/active.img2.jpg", "imgs/tipografika1.jpg"],
         avatar: "imgs/img3.jpg",
+        type: 'imgs',
         active: false
     },
     {
         id: 4,
         name: "smm",
-        media: ["imgs/tipografika4.jpg", "imgs/tipografika4.jpg"],
+        media: ["media/MUSOBAQA tgg(Android).mp4", "media/MUSOBAQA tgg(Android).mp4"],
         avatar: "imgs/img4.jpg",
+        type: 'vedio',
         active: false
     },
     {
         id: 5,
         name: "dasturlash",
-        media: ["imgs/tipografika4.jpg", "imgs/tipografika4.jpg"],
+        media: ["media/xurshid.mp4", "media/MUSOBAQA tgg(Android).mp4"],
         avatar: "imgs/img1.jpg",
+        type: 'vedio',
         active: false
     },
     {
         id: 6,
         name: "suhbat",
-        media: ["imgs/tipografika4.jpg", "imgs/tipografika4.jpg"],
+        media: ["imgs/active.img3.jpg", "imgs/tipografika4.jpg"],
         avatar: "imgs/img3.jpg",
+        type: 'imgs',
         active: false
     }
 ];
@@ -46,47 +53,136 @@ const stories = [
 const elSliderList = document.querySelector('.slider-list');
 
 
-// let elList1 = document.querySelector('#slider1');
-// let elList2 = document.querySelector('#slider2');
-let elList3 = document.querySelector('#slider3');
-
-// elList3.addEventListener('click', openActive);
-
-// function openActive() {
-//     elList1.classList.add('active');
-//     elList3.classList.remove('active');
-//     elList1.innerHTML = `
-//         <div class="item-box">
-//             <img class="slider-img" src="imgs/tipografika1.jpg" alt="img">
-//             <div class="sumbit-box">
-//                 <input type="text" placeholder="Reply to najottalim...">
-//                 <button type="submit">
-//                     <i class='bx bxl-telegram'></i>
-//                 </button>
-//             </div>
-//         </div>
-//     `
-// }
-
 function render(stories) {
     stories.forEach(item => {
         let li = document.createElement('li');
         li.className = 'slider-item';
-        // li[2].classlist.add('active');
-        li.innerHTML = `
+        if(item.type == 'imgs') {
+            li.innerHTML = `
             <div class="item-box">
+                <div class = "before-box">
+                    <span class = "before-span"></span>
+                </div>
+                <div class="user-box">
+                    <div class="user">
+                        <img src="${item.avatar}" alt="najot">
+                        <span>${item.name}</span>
+                    </div>
+                </div>
+                <div class = "imgs-box"></div>
                 <img class="slider-img" src="${item.media[0]}" alt="img">
-                <div class="avatar-box">
-                    <img class="img-avatar" src="${item.avatar}" alt="pic">
-                    <p class="avatar-title">${item.name}</p>
+                <div class="sumbit-box">
+                    <input type="text" placeholder="Reply to najottalim...">
+                    <button type="submit">
+                        <i class='bx bxl-telegram'></i>
+                    </button>
                 </div>
             </div>
         `
+        } else if(item.type == 'vedio') {
+            li.innerHTML = `
+            <div class="item-box">
+                <div class = "before-box">
+                    <span class = "before-span"></span>
+                </div>
+                <div class="user-box">
+                    <div class="user">
+                        <img src="${item.avatar}" alt="najot">
+                        <span>${item.name}</span>
+                    </div>
+                </div>
+                <div class = "imgs-box"></div>
+                <video class="video-style" controls src="${item.media[0]}"></video>
+                <div class="sumbit-box">
+                    <input type="text" placeholder="Reply to najottalim...">
+                    <button type="submit">
+                        <i class='bx bxl-telegram'></i>
+                    </button>
+                </div>
+            </div>
+        `
+        }
         elSliderList.appendChild(li);
     })
 }
 
-// render(stories);
+render(stories);
+
+const elItems = document.querySelectorAll('.slider-item');
+
+function items0() {
+    elItems[0].addEventListener('click', () => {
+        elItems[0].classList.add('active');
+        elItems[1].classList.remove('active');
+        elItems[2].classList.remove('active');
+        elItems[3].classList.remove('active');
+        elItems[4].classList.remove('active');
+        elItems[5].classList.remove('active');
+        elSliderList.style.transform =`translateX(400px)`
+    })
+}
+function items1() {
+    elItems[1].addEventListener('click', () => {
+        elItems[1].classList.add('active');
+        elItems[0].classList.remove('active');
+        elItems[2].classList.remove('active');
+        elItems[3].classList.remove('active');
+        elItems[4].classList.remove('active');
+        elItems[5].classList.remove('active');
+        elSliderList.style.transform =`translateX(180px)`
+    })
+}
+function items2() {
+    elItems[2].addEventListener('click', () => {
+        elItems[2].classList.add('active');
+        elItems[0].classList.remove('active');
+        elItems[1].classList.remove('active');
+        elItems[3].classList.remove('active');
+        elItems[4].classList.remove('active');
+        elItems[5].classList.remove('active');
+        elSliderList.style.transform =`translateX(-25px)`
+    })
+}
+function items3() {
+    elItems[3].addEventListener('click', () => {
+        elItems[3].classList.add('active');
+        elItems[0].classList.remove('active');
+        elItems[1].classList.remove('active');
+        elItems[2].classList.remove('active');
+        elItems[4].classList.remove('active');
+        elItems[5].classList.remove('active');
+        elSliderList.style.transform =`translateX(-230px)`
+    })
+}
+function items4() {
+    elItems[4].addEventListener('click', () => {
+        elItems[4].classList.add('active');
+        elItems[0].classList.remove('active');
+        elItems[1].classList.remove('active');
+        elItems[2].classList.remove('active');
+        elItems[3].classList.remove('active');
+        elItems[5].classList.remove('active');
+        elSliderList.style.transform =`translateX(-440px)`
+    })
+}
+function items5() {
+    elItems[5].addEventListener('click', () => {
+        elItems[5].classList.add('active');
+        elItems[0].classList.remove('active');
+        elItems[1].classList.remove('active');
+        elItems[2].classList.remove('active');
+        elItems[3].classList.remove('active');
+        elItems[4].classList.remove('active');
+        elSliderList.style.transform =`translateX(-650px)`
+    })
+}
+
+items0()
+items1()
+items2()
+items3()
+items4()
+items5()
 
 
 // ================== post slider list ===============
@@ -94,6 +190,16 @@ function render(stories) {
 const leftBtn = document.querySelector('.left__btn');
 const rightBtn = document.querySelector('.right__btn');
 const itemCard = document.querySelectorAll('.slider-item');
+
+rightBtn.addEventListener('click', ()=> {
+    stories.forEach(item => {
+        if(item.id == 1) {
+            items1()
+        } else if (item.id == 2) {
+            items2()
+        }
+    });
+})
 
 let idx = 0;
 
@@ -115,14 +221,6 @@ function postSlider() {
 //     idx--;
 //     postSlider();
 // })
-
-rightBtn.addEventListener('click', () => {
-    elSliderList.style.transform =`translateX(150px)`
-})
-
-leftBtn.addEventListener('click', () => {
-    elSliderList.style.transform =`translateX(-150px)`
-})
 
 
 
